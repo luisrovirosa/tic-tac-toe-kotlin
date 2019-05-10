53cf1c0273a793decafd43c0dd9ce093cd0b0dcf
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-10 15:39:47 +0200

    refactor: reorder code do reduce duplicity

--- src/main/java/TicTacToe.kt
+++ src/main/java/TicTacToe.kt
@@ -12,16 +12,8 @@ class TicTacToe {
             if ("X" == board[i][0] && "X" == board[i][1] && "X" == board[i][2]) {
                 return "X"
             }
-            if ("Y" == board[i][0] && "Y" == board[i][1] && "Y" == board[i][2]) {
-                return "Y"
-            }
-        }
-        for (i in 0..2) {
             if ("X" == board[0][i] && "X" == board[1][i] && "X" == board[2][i]) {
                 return "X"
-            }
-            if ("Y" == board[0][i] && "Y" == board[1][i] && "Y" == board[2][i]) {
-                return "Y"
             }
         }
         if ("X" == board[0][0] && "X" == board[1][1] && "X" == board[2][2]) {
@@ -29,6 +21,14 @@ class TicTacToe {
         }
         if ("X" == board[2][0] && "X" == board[1][1] && "X" == board[0][2]) {
             return "X"
+        }
+        for (i in 0..2) {
+            if ("Y" == board[i][0] && "Y" == board[i][1] && "Y" == board[i][2]) {
+                return "Y"
+            }
+            if ("Y" == board[0][i] && "Y" == board[1][i] && "Y" == board[2][i]) {
+                return "Y"
+            }
         }
         if ("Y" == board[0][0] && "Y" == board[1][1] && "Y" == board[2][2]) {
             return "Y"
