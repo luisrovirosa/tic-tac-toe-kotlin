61d7f11c8a976cb57d3bcba3c4bc44bd78b3ec32
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-10 15:54:04 +0200

    refactor reduce mental complexity

--- src/main/java/TicTacToe.kt
+++ src/main/java/TicTacToe.kt
@@ -51,7 +51,7 @@ class TicTacToe {
         if (player == board[0][0] && player == board[1][1] && player == board[2][2]) {
             return true
         }
-        if (player == board[2][0] && player == board[1][1] && player == board[0][2]) {
+        if (player == board[0][2] && player == board[1][1] && player == board[2][0]) {
             return true
         }
         return false
