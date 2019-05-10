40c5d8c27991b343c67438b3e2743ba781ee772e
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-10 15:46:13 +0200

    refactor: reduce duplicity to Y player

--- src/main/java/TicTacToe.kt
+++ src/main/java/TicTacToe.kt
@@ -11,18 +11,7 @@ class TicTacToe {
         if (hasWonPlayerX("X")){
             return "X"
         }
-        for (i in 0..2) {
-            if ("Y" == board[i][0] && "Y" == board[i][1] && "Y" == board[i][2]) {
-                return "Y"
-            }
-            if ("Y" == board[0][i] && "Y" == board[1][i] && "Y" == board[2][i]) {
-                return "Y"
-            }
-        }
-        if ("Y" == board[0][0] && "Y" == board[1][1] && "Y" == board[2][2]) {
-            return "Y"
-        }
-        if ("Y" == board[2][0] && "Y" == board[1][1] && "Y" == board[0][2]) {
+        if (hasWonPlayerX("Y")){
             return "Y"
         }
 
